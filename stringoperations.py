3352ab53bf4a011209d7604ce5f7cec413ce67d6
Tejesvar <161117175+Tejesvar@users.noreply.github.com>
2024-02-27 11:05:14 +0530

    Update stringoperations.py

--- stringoperations.py
+++ stringoperations.py
@@ -1,19 +1,19 @@
-s=input("enter your string")
+s=input("enter your string: ")
 def countletters(s):
     n=len(s)
-    print("the string is",s)
-    print("number of letters in the string is",n)
+    print("the string is: ",s)
+    print("number of letters in the string is: ",n)
 def capital(s):
     t=s.upper()
-    print("The original string is")
+    print("The original string is:")
     print(s)
-    print("The converted string is")
+    print("The converted string is:")
     print(t)
 def small(s):
     t=s.lower()
-    print("The original string is")
+    print("The original string is:")
     print(s)
-    print("The converted string is")
+    print("The converted string is:")
     print(t)
 def pali(s):
     n=len(s)
@@ -26,26 +26,24 @@ def pali(s):
     else:
         print("entered string is  a not palindrome")
 def repeat(s):
-    t=int(input("how many times the string should repeat"))
+    t=int(input("how many times the string should repeat: "))
     v=s*t
     for i in range(0,len(v),len(s)):
         print(v[i:i+t-1])
 def reverse(s):
     n=len(s)
-    print("The original string is"," ",s) 
     for i in range(n-1,-1,-1):
         print(s[i],end=" ")
 def asccode(s):
     n=len(s)
     for i in range(0,n):
-        print(s[i]," ",ord(s[i]))
+        print(s[i],":",ord(s[i]))
 def vowelcount(s):
-    t=len(s)
     c=0
-    for i in t:
+    for i in s:
         if i=="A" or i=="E" or i=="I" or i=="O" or i=="U" or i=="e" or i=="a" or i=="i" or i=="o" or i=="u":
             c+=1
-    print("number of vowel letters in the string is",c)        
+    print("number of vowel letters in the string is: ",c)        
 op=1
 while op!=8:
     print("1.countletters")
@@ -57,7 +55,7 @@ while op!=8:
     print("7.ascii code")
     print("8.vowel count")
     print("9.quit")
-    op=int(input("select the option which should perform"))
+    op=int(input("select the option which should perform: "))
     if op==1:
         countletters(s)
     elif op==2:
